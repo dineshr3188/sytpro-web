6b6c4adce20d796a15dcb5192b3b7cd3203501a1
Dinesh R <dinesh.r@Dineshs-MacBook-Pro.local>
2025-11-27 14:22:07 +0530

    removed assets

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // If deploying to GitHub Pages, set base: "/repo-name/"
 export default defineConfig({
   plugins: [react()],
-  base: "/",   // Change later when deploying to GitHub Pages
+  base: "/sytpro-web/",   // Change later when deploying to GitHub Pages
 })
