e7bd64d3b0318eb8941ddb1e2c69cf0473d2b740
Dinesh R <dinesh.r@Dineshs-MacBook-Pro.local>
2025-12-01 09:58:01 +0530

    admin dashboard

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // If deploying to GitHub Pages, set base: "/repo-name/"
 export default defineConfig({
   plugins: [react()],
-  base: "/",   // Change later when deploying to GitHub Pages
+  define: {
+      // 🔥 Force the production build to include API base URL
+      'import.meta.env.VITE_API_BASE': JSON.stringify('https://api.sytpro.in/api')
+    }
 })
